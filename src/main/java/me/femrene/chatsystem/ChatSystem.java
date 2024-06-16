a5e73db8f30c7667699bcd67f14968e6c8466f77
FemRene <prorene@outlook.de>
2024-06-17 00:05:56 +0200

    Added README.md and did some little Changes

--- src/main/java/me/femrene/chatsystem/ChatSystem.java
+++ src/main/java/me/femrene/chatsystem/ChatSystem.java
@@ -1,17 +1,45 @@
 package me.femrene.chatsystem;
 
+import me.femrene.chatsystem.listeners.onChat;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChatSystem extends JavaPlugin {
 
+    private static ChatSystem instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+        setConf();
         // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(new onChat(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static ChatSystem getInstance() {
+        return instance;
+    }
+
+    public static String getFromConf(String path) {
+        FileConfiguration config = instance.getConfig();
+        return config.getString(path);
+    }
+
+    private static void setConf() {
+        instance.saveDefaultConfig();
+        FileConfiguration config = instance.getConfig();
+        if (!config.contains("arrow")) {
+            config.set("arrow", "§8»");
+        }
+        if (!config.contains("msg")) {
+            config.set("msg", "%prefix %arrow &7%player: &r%message");
+        }
+        instance.saveConfig();
     }
 }
--- src/main/java/me/femrene/chatsystem/listeners/onChat.java
+++ src/main/java/me/femrene/chatsystem/listeners/onChat.java
@@ -1,2 +1,53 @@
-package me.femrene.chatsystem.listeners;public class onChat {
+package me.femrene.chatsystem.listeners;
+
+import me.femrene.chatsystem.ChatSystem;
+import net.kyori.adventure.text.Component;
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class onChat implements Listener {
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        Player p = e.getPlayer();
+        String prefix = "";
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider != null) {
+            LuckPerms api = provider.getProvider();
+            prefix = api.getUserManager().getUser(p.getUniqueId()).getCachedData().getMetaData().getPrefix();
+        }
+        String[] s = e.getMessage().split(" ");
+        for (int i = 0; i < s.length; i++) {
+            if (Bukkit.getPlayer(s[i]) != null && Bukkit.getPlayer(s[i]).getName().equals(s[i])) {
+                s[i] = "§b@"+s[i]+"§r";
+            } else if (Bukkit.getPlayer(s[i].replace("@","")) != null && Bukkit.getPlayer(s[i].replace("@","")).getName().equals(s[i].replace("@",""))) {
+                s[i] = "§b@"+s[i].replace("@","")+"§r";
+            }
+        }
+        if (prefix != null)
+            prefix = ChatColor.translateAlternateColorCodes('&',prefix);
+        else
+            prefix = "";
+        e.setMessage(String.join(" ",s));
+        e.setCancelled(true);
+        String txt = ChatSystem.getFromConf("msg");
+        txt = txt.replace("%prefix",prefix);
+        txt = txt.replace("%arrow",ChatSystem.getFromConf("arrow"));
+        txt = txt.replace("%player",p.getName());
+        txt = txt.replace("%message",e.getMessage());
+        if (p.hasPermission("chat.important")) {
+            Bukkit.broadcast(Component.text(ChatSystem.getFromConf("arrow")));
+            Bukkit.broadcast(Component.text(ChatColor.translateAlternateColorCodes('&',txt)));
+            Bukkit.broadcast(Component.text(ChatSystem.getFromConf("arrow")));
+        } else if (p.hasPermission("chat.write")) {
+            Bukkit.broadcast(Component.text(ChatColor.translateAlternateColorCodes('&',txt)));
+        }
+    }
+
 }
